2c78290b0b2d5659a2bcc726e73ad4f6b46e55c5
KOMMARANAHALLI MAHALAKSHMI <mahalakshmikommaranahalli9@gmail.com>
2026-09-18 21:56:20 +0530

    Add quiz levels with questions and options

---
+++ script.js
@@ -0,0 +1,93 @@
+const levels = [
+
+    {
+        id: 1,
+        difficulty: "EASY",
+        type: "MCQ",
+
+        question: "What does HTML stand for?",
+
+        options: [
+            "Hyper Text Markup Language",
+            "High Text Machine Language",
+            "Hyper Tool Multi Language",
+            "Home Text Markup Language"
+        ],
+
+        answer: 0
+    },
+
+
+    {
+        id: 2,
+        difficulty: "EASY",
+        type: "OUTPUT",
+
+        question: 'What is the output of: console.log(2 + "2")?',
+
+        options: [
+            "4",
+            "22",
+            "undefined",
+            "NaN"
+        ],
+
+        answer: 1
+    },
+
+
+    {
+        id: 3,
+        difficulty: "MEDIUM",
+        type: "LOGIC",
+
+        question: "Which value represents true in JavaScript?",
+
+        options: [
+            "0",
+            "false",
+            "1",
+            "null"
+        ],
+
+        answer: 2
+    },
+
+
+    {
+        id: 4,
+        difficulty: "HARD",
+        type: "DEBUG",
+
+        question: "Which keyword declares a constant in JavaScript?",
+
+        options: [
+            "var",
+            "let",
+            "const",
+            "constant"
+        ],
+
+        answer: 2
+    },
+
+
+    {
+        id: 5,
+        difficulty: "CRITICAL",
+
+        type: "TRICK",
+
+        question: "Which option is NOT a programming language?",
+
+        options: [
+            "Python",
+            "Java",
+            "HTML",
+            "C++"
+        ],
+
+        answer: 2
+    }
+
+];
